033469d7319b8eee3eca489b3f44bc5c4f83f953
novapixel948-gif <novapixel948@gmail.com>
2026-05-12 00:57:15 +0530

    Create tracking-server.js

---
+++ tracking-server.js
@@ -0,0 +1,11 @@
+{
+  "name": "nexotize-tracker",
+  "version": "1.0.0",
+  "main": "tracking-server.js",
+  "scripts": {
+    "start": "node tracking-server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2"
+  }
+}
